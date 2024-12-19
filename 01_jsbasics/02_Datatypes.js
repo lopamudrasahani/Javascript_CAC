6a8640829525db9c559cfe16bea6fcbec9c7de99
Lopamudra S <72513103+lopamudrasahani@users.noreply.github.com>
2024-12-19 14:28:27 +0000

    datatypes

---
+++ 01_jsbasics/02_Datatypes.js
@@ -0,0 +1,30 @@
+"use strict";//treat all js code as newer version
+
+//alert(3 + 3); //we are using node.js not browser
+
+
+console.log( '10lpa');
+console.log("join us on jan 21st to 24th, 2025");
+console.log(10 
+    +
+    5
+);//code readability should be high
+
+let name="ls";
+let age = 21;
+let isWorking = true;
+
+//primitive datatypes
+// string = "" 
+// symbol = unique
+// BigInt
+// Boolean = true and false
+// number =  2 to power 53
+// null = standalone value
+// undefined
+
+console.log(typeof "ls");
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
+
