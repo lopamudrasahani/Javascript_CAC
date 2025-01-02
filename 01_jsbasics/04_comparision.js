97f4bb107afd820b69790986a0f9a13cd1b75d22
LS <72513103+lopamudrasahani@users.noreply.github.com>
2025-01-02 15:31:14 +0000

    comparisions in js

--- 01_jsbasics/04_comparision.js
+++ 01_jsbasics/04_comparision.js
@@ -1,1 +1,26 @@
-sio
+console.log( 2 > 1);
+console.log( 2 >= 1);
+console.log( 2 < 1);
+console.log( 2 == 1);
+console.log( 2 != 1);
+
+//diff between js and typescript is in TS you can't compare two different datatypes
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);//false
+console.log(null == 0);//false
+console.log(null >= 0);//true
+// Note: the reason is that an equality check(==) and comparisions (>,>=,<,<=) work differently
+// comparisions converts null to a number , treating it as 0,
+// that's why  null >=0 is true and null > 0 is false
+
+console.log(undefined == 0);//false
+console.log(undefined > 0);//false
+console.log(undefined < 0);//false
+
+//*****************************strict equality check === (checking both value and datatypes)*************************
+console.log("2" === 2);//false
+
+
+
