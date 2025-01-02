d89a633f77dd374dbd98d3643f718edd15aef4db
LS <72513103+lopamudrasahani@users.noreply.github.com>
2025-01-02 15:18:49 +0000

    operations in JS

--- 01_jsbasics/03_conversionOperation.js
+++ 01_jsbasics/03_conversionOperation.js
@@ -33,3 +33,29 @@ let someNumber = 34;
 
 let stringNumber = String(someNumber);
 console.log(stringNumber, typeof stringNumber);
+
+//*********************Operations*************************/
+// arithmatic operations: +,-,*,/,%
+
+let str1 = "ls";
+let str2 = "kl";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log( 1 + "2");
+console.log("1" + "2");
+console.log(1 + 2 + "3");
+console.log((3+4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+++gameCounter;//prefix
+console.log(gameCounter);
+gameCounter++;//profix
+console.log(gameCounter);
+
