abfc174adf6c2e1de4c21b46ad42481038719292
LS <72513103+lopamudrasahani@users.noreply.github.com>
2025-01-03 01:37:34 +0000

    stack and heap

--- 01_jsbasics/datatypes-summary.js
+++ 01_jsbasics/datatypes-summary.js
@@ -42,6 +42,9 @@ console.log(myFunction);//[Function: myFunction]
 
 //**************************************stack memory and heap memory*********************************************
 //stack(primitive datatype) and heap(non-primitive or reference datatype)
+// The Stack is used for static memory allocation, primarily for storing primitive types and function calls.
+// It's a simple, last-in, first-out (LIFO) structure, making it very fast to access. 
+// Heap: The Heap is used for dynamic memory allocation, where objects and arrays (non-primitive types) are stored.
 
 let myName = "ls";
 
